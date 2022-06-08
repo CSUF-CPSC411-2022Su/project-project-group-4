6392fb4eae6d282b44b924d2f445caca26d3f68c
GuanlinWang <gary.wang@csu.fullerton.edu>
2022-06-08 11:44:58 -0700

    changed name

---
+++ Forecast/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Forecast
+//
+//  Created by Guanlin Wang on 6/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Forecast/ForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastApp.swift
+//  Forecast
+//
+//  Created by Guanlin Wang on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
