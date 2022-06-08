cd8b2cad22a041308e4635e694ee694c43ea7360
GuanlinWang <gary.wang@csu.fullerton.edu>
2022-06-08 11:14:43 -0700

    Initial Commit

---
+++ Undecided/UndecidedApp.swift
@@ -0,0 +1,17 @@
+//
+//  UndecidedApp.swift
+//  Undecided
+//
+//  Created by Guanlin Wang on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct UndecidedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
