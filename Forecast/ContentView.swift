d3fd1b16897a19d9bf26af322ed3d73c15f31432
Jagadeesh756 <JagadeeshK@csu.fullerton.edu>
2022-06-13 16:49:46 -0700

    initial commit

--- Forecast/ContentView.swift
+++ Forecast/ContentView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var locationname: String = ""
+    @State var locationtemp: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Favorite Locations")
+            GeometryReader { geometry in
+                VStack {
+                        HStack {
+                            Spacer()
+                            TextField("Zurich, Switzerland", text: $locationname).font(.body)
+                            TextField("\t\t\t5° C", text: $locationtemp).font(.title2)
+                            Image(systemName: "star.fill")
+                            Spacer()
+                        }
+                        HStack {
+                            Spacer()
+                            TextField("Nice, France", text: $locationname).font(.body)
+                            TextField("\t\t\t12° C", text: $locationtemp).font(.title2)
+                            Image(systemName: "star.fill")
+                            Spacer()
+                        }
+                    }.frame(height: geometry.size.height / 10)
+                    Spacer()
+               
+        }
+            Button {
+                print("Search for Favorites")
+            } label: {
+                Text("Add Favorites")
+                    .padding(15)
+            }
+            .contentShape(Rectangle())
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
