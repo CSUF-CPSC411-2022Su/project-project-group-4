24052f7d56c6cb63621f23b2fae6a8fd7932ed4b
Jagadeesh756 <JagadeeshK@csu.fullerton.edu>
2022-06-20 22:11:59 -0700

    initial commit

---
+++ Forecast/CityWeather.swift
@@ -0,0 +1,21 @@
+//
+//  CityWeather.swift
+//  Forecast
+//
+//  Created by csuftitan on 6/19/22.
+//
+
+import Foundation
+class Cityweather{
+    var city: String
+    var weather: Double
+    var high,low : Double
+    
+    init(city: String, weather: Double, high: Double, low: Double)  {
+        
+    self.city = city
+    self.weather = weather
+    self.high = high
+    self.low = low
+   }
+}
---
+++ Forecast/Locations.swift
@@ -0,0 +1,12 @@
+//
+//  Locations.swift
+//  Forecast
+//
+//  Created by csuftitan on 6/19/22.
+//
+
+import Foundation
+
+let LocationList = [
+    " Fullerton"
+    ]
