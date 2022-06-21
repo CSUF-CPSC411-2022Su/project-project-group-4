62b09116516516e2cfc0da78bbd65d1747a6d2ed
Sumit-29 <bishnoi680@gmail.com>
2022-06-21 09:24:48 -0700

    Updated and edited some classes and implemented NavigationLink

--- Forecast/ContentView.swift
+++ Forecast/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-    feature3View()
+//    feature3View()
+        NavigationLink(destination: feature3View()) {
+                                Text("CLick here for Humidity, Uv Index and Windspeed report")
+                            }
 
 }
 }
