31cf79cbaa01eda529185430d093000dfd0ba351
Sumit-29 <bishnoi680@gmail.com>
2022-06-27 22:43:07 -0700

    Implemented Tab view and data persistence

--- Forecast/ContentView.swift
+++ Forecast/ContentView.swift
@@ -2,21 +2,51 @@
 //  ContentView.swift
 //  Forecast
 //
-//  Created by Guanlin Wang on 6/8/22.
+//  Created by Sumit Bishnoi on 6/8/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("bgRed") var bgRed: Double = 0
+    @AppStorage("bgGreen") var bgGreen: Double = 0
+    @AppStorage("bgBlue") var bgBlue: Double = 0
+
     var body: some View {
-//    feature3View()
-        NavigationLink(destination: feature3View()) {
-                                Text("CLick here for Humidity, Uv Index and Windspeed report")
-                            }
-
+        
+            
+            VStack{
+                
+                TabView {
+                    
+                     
+                    Text("Welcome Page \n Click on More Report tab to see features")
+                        .multilineTextAlignment(.center)
+                        .tabItem {
+                            Image(systemName: "location")
+                            Text("Welcome")
+                                .bold()
+                        }
+                    
+                    feature3View()
+                        .tabItem {
+                            Image(systemName: "wind")
+                            Text("More Reports")
+                                .bold()
+                        }
+                    
+                    Background()
+                       .tabItem{
+                           Image(systemName: "wrench")
+                           Text("Settings")
+                               }
+                }
+            
+            
+    }
+        
+    }
 }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- Forecast/Feature3.swift
+++ Forecast/Feature3.swift
@@ -2,103 +2,203 @@
 //  Feature3.swift
 //  Forecast
 //
-//  Created by csuftitan on 6/20/22.
+//  Created by Sumit Bishnoi on 6/20/22.
 //
 
 import SwiftUI
 
 struct feature3View: View{
     var x = NecessaryFeatures()
+    
     var body: some View {
-        
-        ZStack {
+        ZStack{
             backgroundGradient
                 .ignoresSafeArea()
+        
+        VStack {
             
+         
             VStack {
-                Text("Weather Reports")
-                    .modifier(HeadingText())
-                    .padding()
-                Image("wind.png")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 25.0, height: 25.0)
-                    VStack{
-                        Spacer()
-                        HStack{
-                            VStack{
-                            Image("wind")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 25.0, height: 25.0)
-                            }
-                            VStack(spacing:1){
-                    Text("Wind Speed = \(x.WindSpeed)mph 🌪")
+                HStack{
+                    Spacer()
+                Rectangle()
+                    .fill(.black)
+                    .frame(width: 5, height: 100)
+                    .opacity(0.5)
+                    
+                
+                Text("Weather Details for this location")
+                        .padding(.leading,1)
+                        .font(.custom("Courier New", size: 30))
+//                        .foreground(Color.white)
+                        .foregroundColor(.white)
+                        .padding()
+                    
+                }.padding()
+                Spacer()
+
+            }
+
+            VStack{
+            HStack{
+                Spacer()
+                VStack {
+                    
+                    Text("Sunrise")
                             .bold()
+                            .shadow(radius: 2)
+                    Image("morning")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                        .shadow(radius: 2)
+                
+                Text( "AM")
+                    .bold()
+                    .shadow(radius: 2)
+                    Spacer()
+                }
+                
+                HStack{
+                    Spacer()
+                Rectangle()
+                    .fill(.blue)
+                    .frame(width: 1, height: 100)
+                    .opacity(0.5)
+                    Spacer()
+                }
+                
+                VStack {
+                    Spacer()
+                    Text("Sunset")
+                            .bold()
+                            .shadow(radius: 2)
+                    Image("sea")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                        .shadow(radius: 2)
+                
+                Text( "PM")
+                    .bold()
+                    .shadow(radius: 2)
+                    Spacer()
+                }
+                Spacer()
+            }.modifier(featureBlock2())
+                
+            // Feature 3
+                Spacer()
+            HStack(spacing:1){
+                Spacer()
+                            VStack {
+                                Spacer()
+                                Image("wind")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                    .shadow(radius: 2)
+                            Text("Wind")
+                                    .bold()
+                                    .shadow(radius: 2)
+                            Text( "\(x.WindSpeed)Mph")
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .shadow(radius: 2)
+                                Spacer()
                             }
                             
-                        }.modifier(BackForWind())
-                    VStack{
-                    Text("Humidity = \(x.Humidity)% 🌧")
-                            .bold()
-                    }.modifier(BackForHum())
-                    VStack{
-                    Text("Uv Index = \(x.UvIndex) ☀️")
-                            .bold()
-                    }.modifier(BackForUV())
-                }
-            
+                            HStack{
+                                Spacer()
+                            Rectangle()
+                                .fill(.blue)
+                                .frame(width: 1, height: 100)
+                                .opacity(0.5)
+                                Spacer()
+                                
+                            }
+                            VStack {
+                                Image("humidity")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                    .shadow(radius: 2)
+                                    
+                                Text("Humidity")
+                                Text("\(bgRed)")
+                                Text("\(bgBlue)")
+                                    .bold()
+                                    .multilineTextAlignment(.center)
+                                    .shadow(radius: 2)
+                            Text( "\(x.Humidity)% ")
+                                    .bold()
+                                    .shadow(radius: 2)
+                                Spacer()
+                            }
+                            
+                            HStack{
+                                Spacer()
+                            Rectangle()
+                                .fill(.blue)
+                                .frame(width: 1, height: 100)
+                                .opacity(0.5)
+                                Spacer()
+                            }
+                            
+                            VStack {
+                                Image("sun")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                    .shadow(radius: 2)
+                                    
+                                Text("UV")
+                                    .bold()
+                                    .multilineTextAlignment(.center)
+                                    .shadow(radius: 2)
+                                Text("Index")
+                                    .bold()
+                                    .multilineTextAlignment(.center)
+                                    .shadow(radius: 2)
+                            Text( "\(x.UvIndex)")
+                                    .bold()
+                                    .shadow(radius: 2)
+                                Spacer()
+                            }
+                Spacer()
+                    }.modifier(featureBlock())
+                
             
         }
-        }}
+        }
+            
+        }
+    }
 }
 
 
-struct HeadingText: ViewModifier {
+struct featureBlock: ViewModifier {
     func body(content: Content) -> some View {
         content
-            .padding(.leading,1)
-            .font(.custom("Courier New", size: 30))
-            .foregroundColor(Color.black)
+        
+            .font(.custom("Courier New", size: 15))
             .padding()
-            .background(Color.teal)
+            .background(Color.white)
             .cornerRadius(10)
+            .shadow(radius: 5)
+            .padding()
     }
 }
-let backgroundGradient = LinearGradient(
-    colors: [Color.blue, Color.white],
-    startPoint: .top, endPoint: .bottom)
 
-struct BackForWind: ViewModifier {
+struct featureBlock2: ViewModifier {
     func body(content: Content) -> some View {
         content
-            
-            .font(.custom("Courier New", size: 25))
+        
+            .font(.custom("Courier New", size: 15))
             .padding()
-            .background(Color.red)
-            .cornerRadius(65)
-            .padding()
-    }
-}
-struct BackForUV: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            
-            .font(.custom("Courier New", size: 25))
-            .padding()
-            .background(Color.yellow)
-            .cornerRadius(65)
-            .padding()
-    }
-}
-struct BackForHum: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            
-            .font(.custom("Courier New", size: 25))
-            .padding()
-            .background(Color.yellow)
-            .cornerRadius(65)
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(radius: 5)
             .padding()
     }
 }
